7a4c52e859842a988b40fa8d4217f561c2ee0237
Shlomi Fish <shlomif@shlomifish.org>
2012-11-28 03:58:21 +0200

    Convert to a reusable function.

--- shlomif-racket-playground/squares-inside-squares.rkt
+++ shlomif-racket-playground/squares-inside-squares.rkt
@@ -5,11 +5,17 @@
 
 (require racket/draw)
 ; (show-pict (hc-append (scale (rotate (rectangle 200 200) 0.2) 0.5) (rectangle 100 30)))
-(let ([angle (degrees->radians 5)])
-(show-pict
-    (inset
+(define (seq start end)
+  (for/list ([i (in-range start end)]) i))
+
+(define (my-squares angle-in-degs count)
+  (let* ([angle (degrees->radians angle-in-degs)]
+         [offset 100]
+         [size (* offset 2)]
+         [my-inset (lambda (offset pict) (inset pict offset offset))])
+    (my-inset offset
       (apply lt-superimpose
               (foldl  {lambda (a result)
                   (cons {scale (rotate (car result) angle)
                          (sqrt (+ (expt angle 2) (expt (- 1 angle) 2)))} result)}
-               (list (inset (rectangle 200 200) -100 -100)) (for/list ([i (in-range 1 20)]) i))) 100 100)))
+               (list (my-inset (- offset) (rectangle size size))) (seq 1 count))))))
