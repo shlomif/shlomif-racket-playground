88c8dcb270ff6424807f2270f8d3922723adc5dd
Shlomi Fish <shlomif@shlomifish.org>
2012-11-28 03:38:30 +0200

    squares-inside-squares is working - yay!.

--- shlomif-racket-playground/squares-inside-squares.rkt
+++ shlomif-racket-playground/squares-inside-squares.rkt
@@ -3,6 +3,13 @@
          (printf "~a~n" x))
 (printf "~a~n" (+ 2 3))
 
-(for ([x (list 1 2 3 4 5 6 7 8 9 10)])
-  (let ((angle (degrees->radians 30)))
-    (scale (rotate (rectangle 200 200) angle) (sqrt (+ (expt angle 2) (expt (- 1 angle) 2))))))
+(require racket/draw)
+; (show-pict (hc-append (scale (rotate (rectangle 200 200) 0.2) 0.5) (rectangle 100 30)))
+(let ([angle (degrees->radians 5)])
+(show-pict
+    (inset
+      (apply lt-superimpose
+              (foldl  {lambda (a result)
+                  (cons {scale (rotate (car result) angle)
+                         (sqrt (+ (expt angle 2) (expt (- 1 angle) 2)))} result)}
+               (list (inset (rectangle 200 200) -100 -100)) (for/list ([i (in-range 1 20)]) i))) 100 100)))
